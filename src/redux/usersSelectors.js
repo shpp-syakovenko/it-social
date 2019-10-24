679b9314ab01604e45394119a3f82232f971c66d
syakovenko <serglife777@gmail.com>
2019-10-24 06:32:22 +0300

    lesson 83

--- src/redux/usersSelectors.js
+++ src/redux/usersSelectors.js
@@ -1,6 +1,15 @@
-export const getUsers = (state) => {
+import {createSelector} from "reselect";
+
+// Простой селектор
+const getUsersSelector = (state) => {
   return state.usersPage.users
 };
+
+// Реселектор который завист от простого селектра getUsersSelector если в простом не чего не поменялось вернет тотже результат
+// если поменялось выполнит сложную операцию
+export const getUsers = createSelector(getUsersSelector,(users) => {
+  return users.filter(u => true);
+});
 
 export const getPageSize = (state) => {
   return state.usersPage.pageSize
